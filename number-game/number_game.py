1f29d27286f3fd9649c4ef32919f611ae65a83c9
Keegan Gladstone <keegangladstone@gmail.com>
2017-09-07 18:52:05 -1000

    catch ValueError exception

--- number-game/number_game.py
+++ number-game/number_game.py
@@ -1,5 +1,6 @@
 import random
 
+#game loop and logic
 def game():
     # generate a random number between 1 and 10
     secret_num = random.randint(1, 10)
@@ -7,25 +8,29 @@ def game():
     tries = 5
     # get a number guess from the player
     while tries != 0:
-        guess = int(input("Guess a number between 1 and 10: "))
+            try:
+                guess = int(input("Guess a number between 1 and 10: "))
 
-        # compare the guess to the secret number
-        if guess == secret_num:
-            print("You got it! My number was {}".format(secret_num))
-            break;
-        elif guess > secret_num:
-            print("Your guess is too high. Try again!")
+            except ValueError:
+                print("{} is not a number".format(guess))
+            # compare the guess to the secret number
+            if guess == secret_num:
+                print("You got it! My number was {}".format(secret_num))
+                break;
+            elif guess > secret_num:
+                print("Your guess is too high. Try again!")
 
-        elif guess < secret_num:
-            print("Your guess is too low. Try again!") 
+            elif guess < secret_num:
+                print("Your guess is too low. Try again!") 
 
-        tries-= 1
+            tries-= 1
 
-        if tries == 0:
-            print("You ran out of tries. You lose!")
+    else:
+        print("You lose! My number was {}".format(secret_num))
 
     return
 
+#game start and end prompt
 while True:
     cont = input("Would you like to play? Type YES or NO: ")
 
@@ -35,6 +40,6 @@ while True:
     else:
         print("Bye.")
         break
-    
+
 
 
