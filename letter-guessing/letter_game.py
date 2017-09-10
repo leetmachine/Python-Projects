de0820970fda581d21c06a33e89fbf6aed6d9a9a
Keegan Gladstone <keegangladstone@gmail.com>
2017-09-10 09:17:16 -1000

    revised with functions

--- letter-guessing/letter_game.py
+++ letter-guessing/letter_game.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 #make a list of words
@@ -15,54 +16,91 @@ words = [
     "blueberry",
     "melon"
 ]
+def welcome():
+    print('WELCOME TO WORD GUESS!')
+    start = input("Press enter to start or Q to quit ").lower()
 
-while True:
-    start  = input('press enter to start, or enter Q to quit: ')
+    if start == 'q':
+        print('bye.')
+        sys.exit()
+    else:
+        return True
 
-    if start.lower() == 'q':
-        break
+def clear():
+    if os.name=='nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-    #pick a random word
-    secret_word = random.choice(words)
-    bad_guesses  = []
-    good_guesses = []
+def draw(bad_guesses, good_guesses, secret_word):
+    clear()
 
-    while len(bad_guesses) < 7 and len(good_guesses) != len(list(secret_word)):
-        #draw spaces
-        for letter in secret_word:
-            if letter in good_guesses:
-                print(letter, end='')
-            else:
-                print('_', end='')
-            
-        print('')
-        print('Strikes: {}/7'.format(len(bad_guesses)))
-        print('')
+    print('Strikes: {}/7'.format(len(bad_guesses)))
+    print('')
 
-        #take guess
+    for letter in bad_guesses:
+        print(letter, end='')
+    print('\n\n')
+
+    for letter in secret_word:
+        if letter in good_guesses:
+            print(letter, end='')
+        else:
+            print('_', end='')
+        
+    print('')
+
+def get_guess(bad_guesses, good_guesses):
+    while True:
         guess = input('Guess a letter: ').lower()
 
         if len(guess) != 1:
             print('You can only guess a single letter!')
-            continue
         elif guess in bad_guesses or guess in good_guesses:
             print("You already guessed that letter.")
-            continue
         elif not guess.isalpha():
             print("You can only guess letters.")
-            continue
-        
+        else:
+            return guess
+
+def play(done):
+    clear()
+    secret_word = random.choice(words)
+    bad_guesses = []
+    good_guesses = []
+
+    while True:
+        draw(bad_guesses,good_guesses, secret_word)
+        guess = get_guess(bad_guesses, good_guesses)
+
         if guess in secret_word:
             good_guesses.append(guess)
-            print("length of guess {}.".format(len(list(secret_word))))
-            print("length of good guesses {}.".format(len(good_guesses)))
-            if len(good_guesses) == len(list(secret_word)):
-                print("Yon win! The word was {}".format(secret_word))
-                break
+            found = True
+            for letter in secret_word:
+                if letter not in good_guesses:
+                    found = False
+            if found:
+                print("You win!")
+                print("The secret word was {}".format(secret_word))
+                done = True
         else:
             bad_guesses.append(guess)
-    
-    else:
-        print("You lose! The secret word was {}".format(secret_word))
-    #draw guessed letters and strikes
-    #print out win/lose
+            if len(bad_guesses) == 7:
+                draw(bad_guesses,good_guesses,secret_word)
+                print("You lost!")
+                print("The secret word was {}".format(secret_word))
+                done = True
+        
+        if done:
+            play_again = input("Play again? Y/n: ").lower()
+            if play_again != 'n':
+                return play(done=False)
+            else:
+                print('bye.')
+                sys.exit()
+
+done = False
+
+while True:
+    welcome()
+    play(done)
